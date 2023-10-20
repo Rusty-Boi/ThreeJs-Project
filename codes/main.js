cfe2de754cff67c792676b2c8dc33235f8d11520
Rusty-Boi <69038549+Rusty-Boi@users.noreply.github.com>
2023-10-20 10:07:05 +0700

    mandatory files

--- codes/main.js
+++ codes/main.js
@@ -1,2 +1,18 @@
 import * as TJS from "./Three JS/build/three.module.js"
 
+//Windows height and width
+const winH = window.innerHeight
+const winW = window.innerWidth
+
+//make scene
+const main_scene = new TJS.Scene()
+
+//make camera PerspectiveCamera(fov, aspect ratio, near, far)
+const cam = new TJS.PerspectiveCamera(90, winH/winW, 0.1, 10000)
+
+//make renderer
+const renderer = new TJS.WebGL1Renderer()
+renderer.setSize(winW, winH)
+renderer.setPixelRatio(window.devicePixelRatio)
+
+document.body.appendChild
---
+++ codes/obj.js
@@ -0,0 +1,5 @@
+import * as TJS from "./Three JS/build/three.module.js"
+
+function makePlanet(){
+    
+}
