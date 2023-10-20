b37150db3f8b189ebe0cc66779101f3ebf5deedb
Rusty-Boi <69038549+Rusty-Boi@users.noreply.github.com>
2023-10-20 20:11:50 +0700

    camera fix

--- codes/main.js
+++ codes/main.js
@@ -9,19 +9,23 @@ const winW = window.innerWidth
 const main_scene = new TJS.Scene()
 
 //make camera PerspectiveCamera(fov, aspect ratio, near, far)
-const cam = new TJS.PerspectiveCamera(90, winH/winW, 0.1, 10000)
-cam.position.setZ(70)
+const cam = new TJS.PerspectiveCamera(70, winW/winH, 0.1, 1000)
+cam.position.setZ(400)
 
 //make objects
 
-const planet1 = makePlanet(15)
-const planet2 = makePlanet(30)
-main_scene.add(planet1)
-main_scene.add(planet2)
+const planet1 = makePlanet(100)
+const planet2 = makePlanet(18)
+const planet3 = makePlanet(20)
+
+planet2.position.setX(180)
+planet3.position.setX(300)
+
+main_scene.add(planet1, planet2, planet3)
 
 //make renderer
-const renderer = new TJS.WebGL1Renderer()
-renderer.setSize(winW, winH)
+const renderer = new TJS.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
 
 document.body.appendChild(renderer.domElement)
@@ -29,6 +33,7 @@ function animate() {
     requestAnimationFrame(animate)
     planet1.rotation.y += 0.005
     planet2.rotation.y += 0.001
+    planet3.rotation.y += 0.001
     renderer.render(main_scene, cam)
 }
 
--- codes/obj.js
+++ codes/obj.js
@@ -1,7 +1,7 @@
 import * as TJS from "./Three JS/build/three.module.js"
 
 export function makePlanet(radius){
-    const sphere = new TJS.SphereGeometry(radius, 20, 20)
+    const sphere = new TJS.SphereGeometry(radius, 30, 30)
     const material = new TJS.MeshBasicMaterial({wireframe:true, color:"#ffffff"})
     const planet = new TJS.Mesh(sphere, material)
     return planet
