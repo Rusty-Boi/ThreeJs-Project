b9f7f663625e7051e54839a811946313df964083
Rusty-Boi <69038549+Rusty-Boi@users.noreply.github.com>
2023-10-20 19:51:47 +0700

    some fixes

--- codes/main.js
+++ codes/main.js
@@ -1,4 +1,5 @@
 import * as TJS from "./Three JS/build/three.module.js"
+import { makePlanet } from "./obj.js"
 
 //Windows height and width
 const winH = window.innerHeight
@@ -9,10 +10,27 @@ const main_scene = new TJS.Scene()
 
 //make camera PerspectiveCamera(fov, aspect ratio, near, far)
 const cam = new TJS.PerspectiveCamera(90, winH/winW, 0.1, 10000)
+cam.position.setZ(70)
+
+//make objects
+
+const planet1 = makePlanet(15)
+const planet2 = makePlanet(30)
+main_scene.add(planet1)
+main_scene.add(planet2)
 
 //make renderer
 const renderer = new TJS.WebGL1Renderer()
 renderer.setSize(winW, winH)
 renderer.setPixelRatio(window.devicePixelRatio)
 
-document.body.appendChild
+document.body.appendChild(renderer.domElement)
+function animate() {
+    requestAnimationFrame(animate)
+    planet1.rotation.y += 0.005
+    planet2.rotation.y += 0.001
+    renderer.render(main_scene, cam)
+}
+
+animate()
+
--- codes/obj.js
+++ codes/obj.js
@@ -1,5 +1,8 @@
 import * as TJS from "./Three JS/build/three.module.js"
 
-function makePlanet(){
-    
+export function makePlanet(radius){
+    const sphere = new TJS.SphereGeometry(radius, 20, 20)
+    const material = new TJS.MeshBasicMaterial({wireframe:true, color:"#ffffff"})
+    const planet = new TJS.Mesh(sphere, material)
+    return planet
 }
