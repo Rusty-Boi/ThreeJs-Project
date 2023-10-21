0438923e5b9a3546c21b7694be0d455b8f0f4f3c
Rusty-Boi <69038549+Rusty-Boi@users.noreply.github.com>
2023-10-21 12:01:03 +0700

    sun and earth Textures

--- codes/main.js
+++ codes/main.js
@@ -1,5 +1,6 @@
 import * as TJS from "./Three JS/build/three.module.js"
-import { makePlanet } from "./obj.js"
+import { makePlanet, spaceTexture } from "./obj.js"
+
 
 //Windows height and width
 const winH = window.innerHeight
@@ -12,16 +13,21 @@ const main_scene = new TJS.Scene()
 const cam = new TJS.PerspectiveCamera(70, winW/winH, 0.1, 1000)
 cam.position.setZ(400)
 
+//object texture
+const earthTexture = new TJS.TextureLoader().load('./assets/textures/earthTexture.png')
+const sunTexture = new TJS.TextureLoader().load('./assets/textures/sunTexture.jpeg')
+
 //make objects
 
-const planet1 = makePlanet(100)
-const planet2 = makePlanet(18)
-const planet3 = makePlanet(20)
+const planet1 = makePlanet(100, sunTexture)
+const planet2 = makePlanet(18, earthTexture)
+// const planet3 = makePlanet(20)
 
 planet2.position.setX(180)
-planet3.position.setX(300)
+// planet3.position.setX(300)
 
-main_scene.add(planet1, planet2, planet3)
+main_scene.background = spaceTexture
+main_scene.add(planet1, planet2)
 
 //make renderer
 const renderer = new TJS.WebGLRenderer()
@@ -34,7 +40,6 @@ function animate() {
     requestAnimationFrame(animate)
     planet1.rotation.y += 0.005
     planet2.rotation.y += 0.001
-    planet3.rotation.y += 0.001
     renderer.render(main_scene, cam)
 }
 
--- codes/obj.js
+++ codes/obj.js
@@ -1,9 +1,13 @@
 import * as TJS from "./Three JS/build/three.module.js"
 
+//background texture
+export const spaceTexture  = new TJS.TextureLoader().load('./assets/textures/spaceTexture.jpg')
+
 //use this function to make planet object
-export function makePlanet(radius){
-    const sphere = new TJS.SphereGeometry(radius, 30, 30)
-    const material = new TJS.MeshBasicMaterial({wireframe:true, color:"#ffffff"})
+
+export function makePlanet(radius, texture_image){
+    const sphere = new TJS.SphereGeometry(radius, 45, 45)
+    const material = new TJS.MeshBasicMaterial({map: texture_image})
     const planet = new TJS.Mesh(sphere, material)
     return planet
 }
