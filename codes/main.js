632d918607b4bbb18cf9bb32d38c494c27088f9f
Rusty-Boi <69038549+Rusty-Boi@users.noreply.github.com>
2023-10-21 19:07:56 +0700

    added mercury mars and saturn textures

--- codes/main.js
+++ codes/main.js
@@ -10,24 +10,40 @@ const winW = window.innerWidth
 const main_scene = new TJS.Scene()
 
 //make camera PerspectiveCamera(fov, aspect ratio, near, far)
-const cam = new TJS.PerspectiveCamera(70, winW/winH, 0.1, 1000)
-cam.position.setZ(400)
+const cam = new TJS.PerspectiveCamera(70, winW/winH, 0.1, 100000)
+cam.position.setZ(750)
+cam.position.setY(400)
+cam.lookAt(0,0,0)
 
 //object texture
 const earthTexture = new TJS.TextureLoader().load('./assets/textures/earthTexture.png')
 const sunTexture = new TJS.TextureLoader().load('./assets/textures/sunTexture.jpeg')
+const mercuryTexture = new TJS.TextureLoader().load('./assets/textures/mercuryTexture.jpg')
+const venusTexture = new TJS.TextureLoader().load('./assets/textures/venusTexture.jpg')
+const marsTexture = new TJS.TextureLoader().load('./assets/textures/marsTexture.jpg')
+const saturnTexture = new TJS.TextureLoader().load('./assets/textures/saturnTexture.jpg')
+const saturnRingTexture = new TJS.TextureLoader().load('./assets/textures/saturnRingTexture.jpg')
 
 //make objects
 
-const planet1 = makePlanet(100, sunTexture)
-const planet2 = makePlanet(18, earthTexture)
+const sun = makePlanet(100, sunTexture)
+const planet_mercury = makePlanet(10, mercuryTexture)
+const planet_venus = makePlanet(14, venusTexture)
+const planet_earth = makePlanet(18, earthTexture)
+const planet_mars = makePlanet(17, marsTexture)
+
 // const planet3 = makePlanet(20)
 
-planet2.position.setX(180)
+planet_earth.position.setX(400)
+planet_mercury.position.setX(200)
+planet_venus.position.setX(300)
+planet_mars.position.setX(500)
 // planet3.position.setX(300)
 
 main_scene.background = spaceTexture
-main_scene.add(planet1, planet2)
+sun.add(planet_mercury, planet_venus, planet_earth, planet_mars)
+main_scene.add(sun)
+
 
 //make renderer
 const renderer = new TJS.WebGLRenderer()
@@ -38,8 +54,11 @@ document.body.appendChild(renderer.domElement)
 //animate objects
 function animate() {
     requestAnimationFrame(animate)
-    planet1.rotation.y += 0.005
-    planet2.rotation.y += 0.001
+    sun.rotation.y += 0.0005  
+    planet_mercury.rotation.y += 0.05
+    planet_earth.rotation.y += 0.005
+    planet_venus.rotation.y += 0.01
+    planet_mars.rotation.y += 0.006
     renderer.render(main_scene, cam)
 }
 
--- codes/obj.js
+++ codes/obj.js
@@ -6,7 +6,7 @@ export const spaceTexture  = new TJS.TextureLoader().load('./assets/textures/spa
 //use this function to make planet object
 
 export function makePlanet(radius, texture_image){
-    const sphere = new TJS.SphereGeometry(radius, 45, 45)
+    const sphere = new TJS.SphereGeometry(radius, 30, 30)
     const material = new TJS.MeshBasicMaterial({map: texture_image})
     const planet = new TJS.Mesh(sphere, material)
     return planet
